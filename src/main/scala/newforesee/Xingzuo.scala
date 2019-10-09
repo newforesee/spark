2bef9e31455c62529b821f7d2aa0ee49e7958784
newforesee <newforesee@outlook.com>
2019-10-09 20:59:12 +0800

    spark

--- src/main/scala/newforesee/Xingzuo.scala
+++ src/main/scala/newforesee/Xingzuo.scala
@@ -1,7 +1,8 @@
 package newforesee
 
 import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+
+import org.joda.time.LocalDate
 
 /**
  * xxx
@@ -10,32 +11,30 @@ import java.util.{Calendar, Date}
 object Xingzuo {
 
   def main(args: Array[String]): Unit = {
-
+    getAgeByBirth("1995-11-15")
   }
 
   def evaluate(birth: String) = {
     if (birth.isEmpty) null
     else {
 
-      getConstellation(birth.split("-")(1), birth.split("-")(2))
+      getConstellation(birth.split("-")(1).toInt, birth.split("-")(2).toInt)
 
     }
 
   }
 
-  def getConstellation(imouth: String, iday: String) = {
-    val month = imouth.toInt
-    val day = iday.toInt
-    var s = "魔羯水瓶双鱼牡羊金牛双子巨蟹狮子处女天秤天蝎射手魔羯"
+  def getConstellation(month: Int, day: Int) = {
     var arr = Array(20, 19, 21, 21, 21, 22, 23, 23, 23, 23, 22, 22);
-    s.substring(month * 2 - (if (day < arr(month - 1)) 2 else 0), month * 2 - (if (day < arr(month - 1)) 2 else 0) + 2)
-
+    "魔羯水瓶双鱼牡羊金牛双子巨蟹狮子处女天秤天蝎射手魔羯".substring(month * 2 - (if (day < arr(month - 1)) 2 else 0), month * 2 - (if (day < arr(month - 1)) 2 else 0) + 2)
   }
 
   def getAgeByBirth(birth:String) ={
     val format = new SimpleDateFormat("yyyy-MM-dd")
-    val date = format.parse(birth)
-    Calendar.getInstance().setTime(new Date())
+    val bir: LocalDate = LocalDate.parse(birth)
+    bir
+//    val date = format.parse(birth)
+//    Calendar.getInstance().setTime(new Date())
 
   }
 }
